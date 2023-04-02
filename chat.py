8680f45e9f91d0808fa60cbdb74c2b64160c1a60
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-04-02 22:44:32 +0200

    feat: use chunks to increase performance

--- chat.py
+++ chat.py
@@ -5,6 +5,7 @@ from utils import log
 from datetime import datetime
 from dotenv import load_dotenv
 
+
 initial_prompt = "You are a useful and intelligent person."
 
 if not os.path.exists("chatlogs"):
@@ -18,28 +19,45 @@ if not os.path.exists(".env"):
 history = []
 
 
-def chat(prompt, engine, temperature):
+def setup():
     load_dotenv(override=True)
     openai.api_key = os.environ["OPENAI_API_KEY"]
-    global history
+
+
+def create_messages(prompt, history):
     messages = [{"role": "system", "content": initial_prompt}]
     for message in history:
         messages.append(message)
     history.append({"role": "user", "content": prompt})
     messages.append({"role": "user", "content": prompt})
+    return messages
+
+
+def call_api(engine, messages, max_tokens, temperature):
     try:
         response = openai.ChatCompletion.create(
             model=engines[engine]["name"],
             messages=messages,
-            max_tokens=engines[engine]["max_tokens"] - len(prompt),
+            max_tokens=max_tokens,
             temperature=temperature,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
         )
         response_text = response["choices"][0]["message"]["content"].strip()
-        log("\n" + "User: " + prompt + "\n" + "Assistant: " + response_text + "\n")
-        history.append({"role": "system", "content": response_text})
         return response_text
     except Exception as e:
         return "I'm sorry, we got an error:" + "\n" + str(e)
+
+
+def chat(prompt, engine, temperature):
+    setup()
+    global history
+    messages = create_messages(prompt, history)
+    max_tokens = engines[engine]["max_tokens"] - len(prompt)
+    chunks = [messages[i : i + max_tokens] for i in range(0, len(messages), max_tokens)]
+    response_chunks = [call_api(engine, chunk, max_tokens, temperature) for chunk in chunks]
+    response_text = "".join(response_chunks)
+    log("\n" + "User: " + prompt + "\n" + "Assistant: " + response_text + "\n")
+    history.append({"role": "system", "content": response_text})
+    return response_text
