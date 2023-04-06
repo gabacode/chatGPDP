db56934e53391dc344ba3d39cb20ae2cad168a85
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-04-06 13:13:52 +0200

    chore: add bash builder

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+delimiters="------------------------------------------------------------"
+
+echo $delimiters
+echo "Starting build..."
+echo $delimiters
+
+# Get current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Create virtualenv
+if [ ! -d "$DIR/venv" ]; then
+    python3 -m venv $DIR/venv
+    echo $delimiters
+    echo "Virtualenv created"
+    echo $delimiters
+fi
+
+# Activate env
+source $DIR/venv/bin/activate
+
+# Install dependencies
+pip install -r $DIR/requirements.txt
+
+# Install pyinstaller
+pip install pyinstaller
+
+# Run pyinstaller
+pyinstaller $DIR/specs/single.spec
+
+# Deactivate virtualenv
+deactivate
+
+echo $delimiters
+echo "Build complete"
+echo $delimiters
