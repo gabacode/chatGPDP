3dd492b165a2e8b64775ec7f26f5639ae4a1f83c
Gabriele Scalici <gaba41@gmail.com>
2023-04-06 16:43:37 +0200

    feat: add windows builder script
    - and updates specs with relative icon path

---
+++ build.bat
@@ -0,0 +1,39 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set delimiters=------------------------------------------------------------
+
+echo !delimiters!
+echo Starting build...
+echo !delimiters!
+
+:: Get current directory
+set "DIR=%~dp0"
+
+:: Create virtualenv
+if not exist "%DIR%\venv" (
+    python -m venv "%DIR%\venv"
+    echo !delimiters!
+    echo Virtualenv created
+    echo !delimiters!
+)
+
+:: Activate env
+call "%DIR%\venv\Scripts\activate.bat"
+
+:: Install dependencies
+pip install -r "%DIR%\requirements.txt"
+
+:: Install pyinstaller
+pip install pyinstaller
+
+:: Run pyinstaller
+pyinstaller "%DIR%\specs\single.spec"
+
+:: Deactivate virtualenv
+deactivate
+
+echo !delimiters!
+echo Build complete
+echo !delimiters!
--- specs/bundle.spec
+++ specs/bundle.spec
@@ -37,7 +37,7 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon='icon.ico'
+    icon='../icon.ico'
 )
 coll = COLLECT(
     exe,
