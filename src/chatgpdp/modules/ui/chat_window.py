c78b76625cc6f9748eb2a87e70b75c14b7e33048
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-04-25 01:10:25 +0200

    feat: easier imports

--- src/chatgpdp/modules/ui/chat_window.py
+++ src/chatgpdp/modules/ui/chat_window.py
@@ -14,11 +14,7 @@ from PyQt5.QtWidgets import (
 
 from chatgpdp.modules import ModelSelector, Temperature, Chatbot
 from chatgpdp.modules.dialogs import AboutDialog, ConfigDialog, PersonalityDialog
-from chatgpdp.modules.ui.components.menu_bar import MenuBar
-from chatgpdp.modules.ui.components.prompt import Prompt
-from chatgpdp.modules.ui.components.chat_log import ChatLog
-from chatgpdp.modules.ui.components.divider import Divider
-from chatgpdp.modules.ui.components.send_button import SendButton
+from chatgpdp.modules.ui.components import MenuBar, ChatLog, Divider, Prompt, SendButton
 from chatgpdp.modules.utils.config import PATHS, load_initial_prompt, shortcuts
 from chatgpdp.modules.utils import Settings, Utilities
 from chatgpdp.modules.threads.chat import ChatThread
---
+++ src/chatgpdp/modules/ui/components/__init__.py
@@ -0,0 +1,5 @@
+from chatgpdp.modules.ui.components.menu_bar import MenuBar
+from chatgpdp.modules.ui.components.chat_log import ChatLog
+from chatgpdp.modules.ui.components.divider import Divider
+from chatgpdp.modules.ui.components.prompt import Prompt
+from chatgpdp.modules.ui.components.send_button import SendButton
